c8ba28edd8a5bf35b3b1a19aea036ec758f5d297
Alfonso <alfonsolasaguirre@gmail.com>
2019-04-05 15:48:27 +0200

    Movile first

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,19 +3,23 @@
   <main>
     <section class="profile">
       <img src="../assets/profilePic.jpg" alt="Progfile img" />
-      <p class="hi">{{ informationTitle }}</p>
-      <p class="information">{{ informationText }}</p>
-      <figure>
-        <div
-          v-for="(info, index) in information"
-          :key="index"
-          @click="changueShowInfo()"
-          class="circle"
-          v-bind:class="{ active: index === textPag }"
-        ></div>
-      </figure>
-      <hr />
-      <SocialMedia />
+      <div class="content">
+        <p class="hi">{{ informationTitle }}</p>
+        <p class="information">{{ informationText }}</p>
+      </div>
+      <footer>
+        <figure>
+          <div
+            v-for="(info, index) in information"
+            :key="index"
+            @click="changueShowInfo()"
+            class="circle"
+            v-bind:class="{ active: index === textPag }"
+          ></div>
+        </figure>
+        <hr />
+        <SocialMedia />
+      </footer>
     </section>
   </main>
 </template>
@@ -62,33 +66,39 @@ main {
     #243b55,
     #141e30
   ); /* Chrome 10-25, Safari 5.1-6 */
-  /* background: radial-gradient(50% 50% at center, #53a0fd 50%, #4f3eeb 200%); */
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%2353a0fd' fill-opacity='0.07'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E"),
     radial-gradient(50% 50% at center, #53a0fd 50%, #4f3eeb 200%);
 }
 
 .profile {
+  width: 75%;
   background-color: white;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   border-radius: 5px;
-  width: 75%;
   justify-self: center;
   align-self: center;
   line-height: 2em;
+  display: grid;
+  grid-template-rows: 12rem 1fr 0.1fr; /* img content footer */
 }
 
 img {
-  width: 28%;
-  height: 28%;
+  width: 150px;
+  height: 150px;
   margin-top: 1rem;
   border-radius: 50%;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.13), 0 6px 20px 0 rgba(0, 0, 0, 0.082);
+  justify-self: center;
+}
+
+.content {
+  align-self: center;
 }
 
 .hi {
   font-size: 2em;
   line-height: 0;
-  margin-top: 1rem;
+  margin: 0;
 }
 
 figure {
@@ -116,8 +126,6 @@ figure {
 }
 
 .arrow {
-  /* color: #5778f3; */
-  /* transition: transform 0.3s ease-in-out; */
   animation: bounce 4s infinite;
   font-size: 25px;
   cursor: pointer;
@@ -147,9 +155,17 @@ hr {
 }
 
 @media (max-width: 600px) {
+  .profile {
+    grid-template-rows: 0.1fr 1.5fr 0.1fr; /* img content footer */
+    line-height: 1.5em;
+  }
   img {
-    width: 45%;
-    height: 45%;
+    width: 110px;
+    height: 110px;
+    margin-bottom: 20px;
+  }
+  .hi {
+    padding-bottom: 8px;
   }
 }
 </style>
